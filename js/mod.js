366524832e00144959f01d8c2d37cda4e5440c6d
silvershadowx3 <thekingofpandas@gmail.com>
2022-02-21 21:13:46 -0600

    Update mod.js
    watch me give up and forget this exists in like a week lol
    
    (renamed some things, gave project a name. basic tutorial-level stuff)

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Build Tree",
+	id: "OverTheRevolution",
+	author: "SilverShadowX3",
+	pointsName: "martian warlord schemes",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
