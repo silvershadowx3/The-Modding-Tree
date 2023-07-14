a930de8f5f03b3f9dfb15d2b5fc2d1e6222ea388
silvershadowx3 <thekingofpandas@gmail.com>
2023-07-14 16:21:05 -0500

    Update layers.js
    added new layer, got bored halfway thru

--- js/layers.js
+++ js/layers.js
@@ -38,3 +38,43 @@ addLayer("p", {
         },
     },
 })
+addLayer("n", {
+    name: "nebula gas", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "N", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#99cc3b",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "Nebula Gas", // Name of prestige currency
+    baseResource: "Pandora's Boxes", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Bottles",
+            description: "Store the gas coming from the wall in small plastic bottles. You aren't sure what, but you feel they could be used for something...",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "Agitation",
+            description: "Exposure to the gas seems to cause agitation in humans. Interesting...",
+            cost: new Decimal(2),
+        },
+    },
+})
